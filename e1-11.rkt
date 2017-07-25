9de46022099646bac9094dfbc77fcc87a795918d
Allvitende <broschroeder@gmail.com>
2017-07-25 11:55:09 -0500

    remove extra file

--- e1-11.rkt
+++ e1-11.rkt
@@ -6,10 +6,9 @@
         (else (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3)))))))
 
 ;Iterative
+(define (f n) (f-iter n 0 1 2))
+
 (define (f-iter i a b c)
   (cond ((< i 0) i)
         ((= i 0) a)
         (else (f-iter (- i 1) b c (+ c (* 2 b) (* 3 a))))))
-
-(f 14)
-(f-iter 14 0 1 2)
